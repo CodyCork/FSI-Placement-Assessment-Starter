641bdb23de13fe5caec5540be4c88924122ca4f7
CodyCork <codycork.sfg@outlook.com>
2022-02-16 20:30:16 -0600

    Test Partially Completed

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,5 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+//My name!
+let yourName = "Cody Cork"
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
@@ -10,18 +8,59 @@ let sugar = 0 // Sugar Sprinkle
 
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
 
-// Code to update name display
-credit.textContent = `Created by ${yourName}`
+//Cookie Quantity Variables
+let gbQuantityTd = document.querySelector('#qty-gb');
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+let ccQuantityTd = document.querySelector('#qty-cc');
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+let sugarQuantityTd = document.querySelector('#qty-sugar');
+
+//Cookie Quantity Total Variable and array
+let totalCookieQuantity = [gb, cc, sugar];
+document.querySelector('#qty-total')
+console.log(totalCookieQuantity)
+
+//Add and Minus Ginger-Bread Cookies
+const gbPlusBtn = document.querySelector('#add-gb');
+gbPlusBtn.addEventListener('click', function addGb() {
+    gb = gb + 1;
+    gbQuantityTd.textContent = gb
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+const gbMinusBtn = document.querySelector('#minus-gb');
+gbMinusBtn.addEventListener('click', function minusGb() {
+    gb = gb - 1;
+    gbQuantityTd.textContent = gb
+})
+
+//Add and Minus Choclate-Chip Cookies
+const ccPlusBtn = document.querySelector('#add-cc');
+ccPlusBtn.addEventListener('click', function addCc() {
+    cc = cc + 1;
+    ccQuantityTd.textContent = cc
+})
+const ccMinusBtn = document.querySelector('#minus-cc');
+ccMinusBtn.addEventListener('click', function minusCc() {
+    cc = cc - 1;
+    ccQuantityTd.textContent = cc
+})
+
+//Add and Minus Sugar-Cookies
+const sugarPlusBtn = document.querySelector('#add-sugar');
+sugarPlusBtn.addEventListener('click', function addSugar() {
+    sugar = sugar + 1;
+    sugarQuantityTd.textContent = sugar
+})
+const sugarMinusBtn = document.querySelector('#minus-sugar');
+sugarMinusBtn.addEventListener('click', function minusSugar() {
+    sugar = sugar - 1;
+    sugarQuantityTd.textContent = sugar
+})
+
+//For loop for total cookie quantity
+for (let i = 0; i < totalCookieQuantity.length; i++) {
+    const total = totalCookieQuantity[i];
+        totalCookieQuantity.textContent = gbQuantityTd.textContent + ccQuantityTd.textContent + sugarQuantityTd.textContent;
+        
+}
